0d6a576d603686c4b7cd188100a9ee92917d2e8b
Tobiaspoe <167797497+Tobiaspoe@users.noreply.github.com>
2025-04-21 10:25:20 +0000

    update v12

--- backend/app.py
+++ backend/app.py
@@ -15,13 +15,14 @@ app = FastAPI()
 
 # Allow frontend & localhost
 origins = [
-    "https://lemon-wave-07946280f.6.azurestaticapps.net"
+    "http://localhost:5173",  # local dev
+    "https://lemon-wave-07946280f.6.azurestaticapps.net"  # deployed frontend
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
